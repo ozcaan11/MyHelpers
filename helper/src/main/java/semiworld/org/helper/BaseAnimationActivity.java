fc1b87cc189b63d876b8d366d09ce1f821422129
ozcaan11 <ozcanyd@gmail.com>
2017-06-24 16:13:55 +0300

    added animation for activity

---
+++ helper/src/main/java/semiworld/org/helper/BaseAnimationActivity.java
@@ -0,0 +1,42 @@
+package semiworld.org.helper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created on 24.06.2017.
+ */
+
+public class BaseAnimationActivity extends AppCompatActivity {
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransitionExit();
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+        overridePendingTransitionEnter();
+    }
+
+    /**
+     * Overrides the pending Activity transition by performing the "Enter" animation.
+     */
+    protected void overridePendingTransitionEnter() {
+        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+    }
+
+    /**
+     * Overrides the pending Activity transition by performing the "Exit" animation.
+     */
+    protected void overridePendingTransitionExit() {
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransitionExit();
+    }
+}
